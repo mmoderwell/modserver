fc72dcb1b3c880d1e070ff522a7eddd36c1646b2
Matt Moderwell <mmoderwell@gmail.com>
2017-11-09 10:07:59 -0600

    Backend changes

--- backend/controllers/light_controller.js
+++ backend/controllers/light_controller.js
@@ -5,31 +5,35 @@ module.exports = {
 		res.send('Hello! Welcome to light controller');
 	},
 	toggle(req, res) {
-		const { name, room, action } = req.params;
+		const { name, action } = req.params;
 
 		const options = {
 			hostname: '10.0.0.208',
 			port: 80,
-			path: `/${room}/${name}/${action}`,
+			path: `/${name}/${action}`,
 			method: 'GET',
 		};
 		const http_req = http.request(options, (http_res) => {
-			console.log(`STATUS: ${http_res.statusCode}`);
-			console.log(`HEADERS: ${JSON.stringify(http_res.headers)}`);
+			//console.log(`Status: ${http_res.statusCode}`);
+			//console.log(`Headers: ${JSON.stringify(http_res.headers)}`);
 			http_res.setEncoding('utf8');
 			http_res.on('data', (chunk) => {
-				console.log(`BODY: ${chunk}`);
+				let body = JSON.parse(chunk);
+				const status = ['off', 'on'];
+				console.log(`Relay ${name} is now ${status[body[name]]}.`);
 			});
 			http_res.on('end', () => {
 				console.log('No more data in response.');
+				res.header("Access-Control-Allow-Origin", "*");
+				let return_json = { "name": name, "action": action }
+				res.send(JSON.stringify(return_json));
 			});
 		});
 
 		http_req.on('error', (e) => {
-			console.error(`problem with request: ${e.message}`);
+			console.error(`There was a problem with that request: ${e.message}`);
 		});
 		http_req.end();
-		res.header("Access-Control-Allow-Origin", "*");
-		res.send(`Turning the ${name} lights in the ${room} ${action}.`);
+
 	}
 }
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,5 +2,5 @@ const light_controller = require('../controllers/light_controller.js');
 
 module.exports = (app) => {
 	app.get('/api/hello', light_controller.hello);
-	app.get('/api/lights/:room/:name/:action', light_controller.toggle);
+	app.get('/api/lights/:name/:action', light_controller.toggle);
 }
