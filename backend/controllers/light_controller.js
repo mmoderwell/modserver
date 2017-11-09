7f55af27e8291a5efd5a0bde7ea7f76518256a39
mmoderwell <mmoderwell@gmail.com>
2017-11-09 13:26:23 -0600

    Added new route and logic for getting light status

--- backend/controllers/light_controller.js
+++ backend/controllers/light_controller.js
@@ -4,6 +4,39 @@ module.exports = {
 	hello(req, res) {
 		res.send('Hello! Welcome to light controller');
 	},
+	status(req, res) {
+		let body;
+
+		const options = {
+			hostname: '10.0.0.208',
+			port: 80,
+			path: `/status`,
+			method: 'GET',
+		};
+		const http_req = http.request(options, (http_res) => {
+			//console.log(`Status: ${http_res.statusCode}`);
+			//console.log(`Headers: ${JSON.stringify(http_res.headers)}`);
+			http_res.setEncoding('utf8');
+			http_res.on('data', (chunk) => {
+				body = JSON.parse(chunk);
+				console.log(body);
+			});
+			http_res.on('end', () => {
+				console.log('No more data in response.');
+				res.header("Access-Control-Allow-Origin", "*");
+				//let return_json = { "name": name, "action": action, "success": true };
+				res.send(JSON.stringify(body));
+			});
+		});
+
+		http_req.on('error', (e) => {
+			console.error(`There was a problem with the request: ${e.message}`);
+			res.header("Access-Control-Allow-Origin", "*");
+			let return_json = { "success": false };
+			res.send(return_json);
+		});
+		http_req.end();
+	},
 	toggle(req, res) {
 		const { name, action } = req.params;
 
@@ -25,15 +58,17 @@ module.exports = {
 			http_res.on('end', () => {
 				console.log('No more data in response.');
 				res.header("Access-Control-Allow-Origin", "*");
-				let return_json = { "name": name, "action": action }
+				let return_json = { "name": name, "action": action, "success": true };
 				res.send(JSON.stringify(return_json));
 			});
 		});
 
 		http_req.on('error', (e) => {
-			console.error(`There was a problem with that request: ${e.message}`);
+			console.error(`There was a problem with the request: ${e.message}`);
+			res.header("Access-Control-Allow-Origin", "*");
+			let return_json = { "name": name, "action": action, "success": false };
+			res.send(return_json);
 		});
 		http_req.end();
-
 	}
 }
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,5 +2,6 @@ const light_controller = require('../controllers/light_controller.js');
 
 module.exports = (app) => {
 	app.get('/api/hello', light_controller.hello);
+	app.get('/api/lights/status', light_controller.status);
 	app.get('/api/lights/:name/:action', light_controller.toggle);
 }
