172b6fe99ff0170b328b93d06e3cfa86abe3ae41
mmoderwell <mmoderwell@gmail.com>
2017-11-09 13:27:19 -0600

    Initial light status and progress light work

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,24 +8,47 @@ two.addEventListener('click', api_call);
 three.addEventListener('click', api_call);
 four.addEventListener('click', api_call);
 
-function api_call(location, name) {
+function api_call() {
+
 	let action;
-	if (this.checked) action = 'on'
-	else action = 'off';
+	if (this.checked) {
+		action = 'on';
+		this.nextSibling.nextSibling.classList.remove('success');
+		this.nextSibling.nextSibling.classList.remove('waiting');
+	} else {
+		action = 'off';
+	}
+
+	//disable checkbox so that no more request are made until this finishes
+	this.disabled = true;
+	this.nextSibling.nextSibling.classList.add('waiting');
 
 	let http_req = new XMLHttpRequest();
 	http_req.overrideMimeType('application/json');
 	http_req.addEventListener("load", res_listen);
-	http_req.onerror = () => this.nextSibling.nextSibling.classList.add('failure');
+
+	http_req.onerror = (e) => {
+		this.nextSibling.nextSibling.classList.remove('waiting');
+		this.nextSibling.nextSibling.classList.add('failure');
+	};
+
 	http_req.open('GET', `http://localhost:8000/api/lights/${this.id}/${action}`, true);
 	//http.setRequestHeader('Content-Type', 'application/json');
 	http_req.send(null);
 }
 
 function res_listen() {
+
 	let response = JSON.parse(this.responseText);
 	let element = document.getElementById(response.name);
 
-	if (this.status == 200) element.nextSibling.nextSibling.classList.add('success')
-	else element.nextSibling.nextSibling.classList.add('failure');
+	if (this.status == 200 && response.success) {
+		element.nextSibling.nextSibling.classList.remove('waiting');
+		element.nextSibling.nextSibling.classList.add('success');
+		element.disabled = false;
+	} else {
+		element.nextSibling.nextSibling.classList.remove('waiting');
+		element.nextSibling.nextSibling.classList.add('failure');
+		console.log('There was an erorr.');
+	}
 }
