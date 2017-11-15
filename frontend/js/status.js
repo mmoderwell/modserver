9c620b899770d0b43ee1b5c6b62c31b8500185e8
mmoderwell <mmoderwell@gmail.com>
2017-11-14 23:40:52 -0600

    Frontend error handling

--- frontend/js/status.js
+++ frontend/js/status.js
@@ -1,11 +1,18 @@
-let relay_status = function() {
+let status_display = document.getElementById('status');
+let numbers = ['one', 'two', 'three', 'four'];
 
+let relay_status = function() {
 	let http_req = new XMLHttpRequest();
 	http_req.overrideMimeType('application/json');
 	http_req.addEventListener("load", res_listen);
 
-	http_req.onerror = (e) => {
-		this.nextSibling.nextSibling.classList.add('failure');
+	http_req.onerror = () => {
+		//this.nextSibling.nextSibling.classList.add('failure');
+		status_display.innerHTML = 'Unable to connect to server.';
+		document.querySelector('.box').classList.add('no_connection');
+		for (let i = 0; i < 4; i++) {
+			document.getElementById(numbers[i]).disabled = true;
+		}
 	};
 
 	http_req.open('GET', `http://localhost:8000/api/lights/status`, true);
@@ -18,14 +25,33 @@ relay_status.call();
 function res_listen() {
 
 	let response = JSON.parse(this.responseText);
+	console.log(response);
 	let element = document.getElementById(response.one);
 
 	if (this.status == 200) {
-		let numbers = ['one', 'two', 'three', 'four'];
-		for (let i = 0; i < 4; i++) {
-			if (response[numbers[i]] == 1) {
-				document.getElementById(numbers[i]).checked = true;
+		if (response.success) {
+			status_display.innerHTML = 'Connected.';
+			for (let i = 0; i < 4; i++) {
+				if (response[numbers[i]] == 1) {
+					document.getElementById(numbers[i]).checked = true;
+				}
+			}
+		} else {
+			status_display.innerHTML = 'Connected to server. Hardware offline.';
+			for (let i = 0; i < 4; i++) {
+				document.getElementById(numbers[i]).disabled = true;
 			}
 		}
+	}
+}
+
+function waiting(unconnected) {
+	let output = 'Connecting to server';
+	for (let i = 0; i < 4; i++) {
+		setTimeout(function() {
+			output + '.';
+			status_display.innerHTML = output;
+			console.log('Done');
+		}, 1000);
 	}
 }
