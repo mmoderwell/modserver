2bcadfe0d5800303f03aea083fbd1eaf5544f92a
mmoderwell <mmoderwell@gmail.com>
2017-11-19 22:03:02 -0600

    Initial commit

---
+++ backend/models/sensor.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sensorSchema = new Schema({
+	value: Number,
+	time: { type: Date, default: Date.now }
+});
+
+const sensor = mongoose.model('sensor', sensorSchema);
+
+module.exports = sensor;
