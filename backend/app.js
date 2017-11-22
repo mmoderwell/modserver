231664967f8c584037d7250a21c668a68e6fa09c
mmoderwell <mmoderwell@gmail.com>
2017-11-21 20:23:41 -0600

    Added body-parser

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
+const bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
+//connect to database hosted on raspberry pi
 mongoose.connect('mongodb://10.0.0.228/modserver')
 	.then(() => console.log('Connected to modserver database.'));
 
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
