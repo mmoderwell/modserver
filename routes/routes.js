55ea37e313ad214748f603566356be65e11111e9
Matt Moderwell <mmoderwell@gmail.com>
2018-08-29 20:14:08 -0500

    Added routes to control lights power

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const relay_controller = require('../controllers/relay_controller.js');
+const light_controller = require('../controllers/light_controller.js');
 const sensor_controller = require('../controllers/sensor_controller.js');
 const status_controller = require('../controllers/status_controller.js');
-
 module.exports = (app) => {
 	//backend
 	app.get('/api/hello', relay_controller.hello);
@@ -9,6 +9,10 @@ module.exports = (app) => {
 	app.get('/api/relay/status', relay_controller.status);
 	app.get('/api/relay/:name/:action', relay_controller.toggle);
 	app.post('/api/sensor/lookup', sensor_controller.lookup);
+	app.get('/api/lights/status', light_controller.status);
+	app.get('/api/lights/off', light_controller.off);
+	app.get('/api/lights/on', light_controller.on);
+	app.get('/api/lights/toggle', light_controller.toggle);
 	//frontend
 	app.get('/', (req, res) => {
 		res.render('../views/index.ejs');
