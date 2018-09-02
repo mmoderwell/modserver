971145d5a6bd31b6130f821b4194021c884d2251
Matt Moderwell <mmoderwell@gmail.com>
2018-09-01 21:43:16 -0500

    Cleaned up some status functions

--- public/js/status.js
+++ public/js/status.js
@@ -16,42 +16,41 @@ let relay_status = function() {
 	//send GET request to this route on Node server
 	http_req.open('GET', '/api/relay/status', true);
 	http_req.send(null);
-};
 
-function res_listen() {
+	function res_listen() {
+		//response recieved back from Node server
+		let response = JSON.parse(this.responseText);
+		console.log(response);
+		//will always be connected to Node server since page is now served from same server
+		if (this.status == 200) {
+			//hardware is connected to server
+			if (response.success) {
+				relay_display.innerHTML = 'Connected.';
+				document.querySelector('.box').classList.remove('no_connection');
 
-	//response recieved back from Node server
-	let response = JSON.parse(this.responseText);
-	console.log(response);
-	//will always be connected to Node server since page is now served from same server
-	if (this.status == 200) {
-		//hardware is connected to server
-		if (response.success) {
-			relay_display.innerHTML = 'Connected.';
-			document.querySelector('.box').classList.remove('no_connection');
-
-			for (let i = 0; i < 4; i++) {
-				//enable the sliders since everything is connected
-				document.getElementById(numbers[i]).disabled = false;
-				// check the slider if the relay is on
-				if (response[numbers[i]] == 1) {
-					document.getElementById(numbers[i]).checked = true;
+				for (let i = 0; i < 4; i++) {
+					//enable the sliders since everything is connected
+					document.getElementById(numbers[i]).disabled = false;
+					// check the slider if the relay is on
+					if (response[numbers[i]] == 1) {
+						document.getElementById(numbers[i]).checked = true;
+					}
 				}
 			}
+			//hardware is not connected to Node server
+			else {
+				relay_display.innerHTML = 'Connected to server. Relays offline.';
+			}
+		} else {
+			console.log(this.responseText);
 		}
-		//hardware is not connected to Node server
-		else {
-			relay_display.innerHTML = 'Connected to server. Relays offline.';
-		}
-	} else {
-		console.log(this.responseText);
 	}
-}
+};
 
 let hardware_status = function() {
 	let http_req = new XMLHttpRequest();
 	http_req.overrideMimeType('application/json');
-	http_req.addEventListener('load', hardware_res_listen);
+	http_req.addEventListener('load', res_listen);
 
 	//if there is an error with sending request to server, let user know
 	http_req.onerror = () => {
@@ -61,26 +60,24 @@ let hardware_status = function() {
 	//send GET request to this route on Node server
 	http_req.open('GET', '/api/status', true);
 	http_req.send(null);
+
+	function res_listen() {
+		//response recieved back from Node server
+		let devices = JSON.parse(this.responseText);
+		devices.forEach((device) => {
+			if (device.hostname == '10.0.0.208' && device.alive) {
+				relay_status.call();
+			} else {
+				relay_display.innerHTML = 'Connected to server. Relays offline.';
+			}
+		});
+	}
 };
-
-hardware_status.call();
-
-function hardware_res_listen() {
-	//response recieved back from Node server
-	let devices = JSON.parse(this.responseText);
-	devices.forEach((device) => {
-		if (device.hostname == '10.0.0.208' && device.alive) {
-			relay_status.call();
-		} else {
-			relay_display.innerHTML = 'Connected to server. Relays offline.';
-		}
-	});
-}
 
 let db_status = function() {
 	let http_req = new XMLHttpRequest();
 	http_req.overrideMimeType('application/json');
-	http_req.addEventListener('load', db_res_listen);
+	http_req.addEventListener('load', res_listen);
 
 	//if there is an error with sending request to server, let user know
 	http_req.onerror = () => {
@@ -90,26 +87,27 @@ let db_status = function() {
 	//send GET request to this route on Node server
 	http_req.open('GET', '/api/database/status', true);
 	http_req.send(null);
-};
 
-db_status.call();
+	function res_listen() {
 
-function db_res_listen() {
-
-	//response recieved back from Node server
-	let response = JSON.parse(this.responseText);
-	//console.log(response);
-	//will always be connected to Node server since page is now served from same server
-	if (this.status == 200) {
-		//hardware is connected to server
-		if (response.connected === 1) {
-			database = true;
-			temp_display.innerHTML = 'Connected to database.';
-			//document.querySelector('.box').classList.remove('no_connection');
-		}
-		//Database is not connected to Node server
-		else {
-			temp_display.innerHTML = 'Database offline.';
+		//response recieved back from Node server
+		let response = JSON.parse(this.responseText);
+		//console.log(response);
+		//will always be connected to Node server since page is now served from same server
+		if (this.status == 200) {
+			//hardware is connected to server
+			if (response.connected === 1) {
+				database = true;
+				temp_display.innerHTML = 'Connected to database.';
+				//document.querySelector('.box').classList.remove('no_connection');
+			}
+			//Database is not connected to Node server
+			else {
+				temp_display.innerHTML = 'Database offline.';
+			}
 		}
 	}
-}
+};
+
+hardware_status.call();
+db_status.call();
